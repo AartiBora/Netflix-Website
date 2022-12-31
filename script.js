68a0e0faffedc912218fa7aaecfd9751b2efaaa3
AartiBora <121597598+AartiBora@users.noreply.github.com>
2022-12-31 12:33:17 +0530

    Add files via upload
    This is my second repository (Netflix Clone)

---
+++ script.js
@@ -0,0 +1,63 @@
+
+  function showA() {
+    let p = document.getElementById('para1')
+    let b = document.getElementById('btnA')
+    if (p.style.display != "none") {
+      p.style.display = "none"
+    }
+    else {
+      p.style.display = "block"
+    }
+  }
+
+  function showB() {
+    let p2 = document.getElementById('para2')
+    let b2 = document.getElementById('btnB')
+    if (p2.style.display != "none") {
+      p2.style.display = "none"
+    }
+    else {
+      p2.style.display = "block"
+    }
+  }
+ function showC() {
+    let p3 = document.getElementById('para3')
+    let b3 = document.getElementById('btnC')
+    if (p3.style.display != "none") {
+      p3.style.display = "none"
+    }
+    else {
+      p3.style.display = "block"
+    }
+  }
+
+  function showD() {
+    let p4= document.getElementById('para4')
+    let b4 = document.getElementById('btnD')
+    if (p4.style.display != "none") {
+      p4.style.display = "none"
+    }
+    else {
+      p4.style.display = "block"
+    }
+  }
+function showE() {
+    let p5= document.getElementById('para5')
+    let b5 = document.getElementById('btnE')
+    if (p5.style.display != "none") {
+      p5.style.display = "none"
+    }
+    else {
+      p5.style.display = "block"
+    }
+  }
+function showF() {
+    let p6= document.getElementById('para6')
+    let b6 = document.getElementById('btnF')
+    if (p6.style.display != "none") {
+      p6.style.display = "none"
+    }
+    else {
+      p6.style.display = "block"
+    }
+  }
